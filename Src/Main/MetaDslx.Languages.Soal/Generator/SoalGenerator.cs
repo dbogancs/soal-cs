d8781040332be3dc97dc8d29412c1c630f938704
Bogáncs Dániel Sándor <d.bogancs@gmail.com>
2017-03-19 12:43:07 +0100

    GitHub test using

--- Src/Main/MetaDslx.Languages.Soal/Generator/SoalGenerator.cs
+++ Src/Main/MetaDslx.Languages.Soal/Generator/SoalGenerator.cs
@@ -11,7 +11,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//GitProba
 namespace MetaDslx.Languages.Soal
 {
     public class SoalGenerator
--- Src/Main/MetaDslx.Languages.Soal/Generator/SoalGeneratorErrorCode.cs
+++ Src/Main/MetaDslx.Languages.Soal/Generator/SoalGeneratorErrorCode.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//GitProba
 namespace MetaDslx.Languages.Soal.Generator
 {
     public class SoalGeneratorErrorCode : ErrorCode
